69d02a56ef3ba2d7a5ea34bfc4bb792e0e0e4d88
noideazzz <1154165252@qq.com>
2026-05-22 17:53:39 +0800

    fix: 解除密码强度校验 + 修复日历编辑time-picker Date类型bug

--- DjangoProject2/settings.py
+++ DjangoProject2/settings.py
@@ -93,18 +93,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    # 教学场景全部禁用，允许任意强度密码
 ]
 
 
